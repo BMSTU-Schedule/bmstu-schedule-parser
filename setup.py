f955c1015745f0f75a43274ca5278aa23cbe6d5c
George <gabolaev98@gmail.com>
2018-04-17 11:57:55 +0300

    Added markdown project description

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='bmstu-schedule',
-    version='0.1.1-beta-7',
+    version='0.1.1-beta-10',
     author='George Gabolaev',
     author_email='gabolaev98@gmail.com',
     url='https://github.com/gabolaev/bmstuSchedule',
     license='MIT',
     python_requires='>=3.5',
+    long_description_content_type='text/markdown',
     description='BMSTU EU Schedule iCal parser',
+    long_description=open('README.md').read(),
     packages=['bmstu_schedule', 'bmstu_schedule.configs'],
     install_requires=[
         'lxml',
