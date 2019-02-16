3a8311cbfc4b877734e68b1105b491db65a9b6ad
George Gabolaev <gabolaev98@gmail.com>
2019-02-16 15:17:54 +0300

    added physical culture lesson keyregex

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='bmstu-schedule',
-    version='1.4.2',
+    version='1.4.3',
     author='George Gabolaev',
     author_email='gabolaev98@gmail.com',
     url='https://github.com/BMSTU-Schedule/bmstu-schedule-parser',
